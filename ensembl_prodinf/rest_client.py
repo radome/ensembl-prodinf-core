b06b8c19d45277f5abd886fa11f1deb90c264482
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-02-15 10:33:03 +0000

    Typo and PEP updates

--- ensembl_prodinf/rest_client.py
+++ ensembl_prodinf/rest_client.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python
-import logging
-import requests
 import argparse
+import logging
 import time
-from server_utils import assert_http_uri
+
+import requests
 from requests.exceptions import HTTPError
 
+from server_utils import assert_http_uri
 
 
 def retry_requests(test_func):
     """
     Decorator for retrying calls to API in case of Network issues
-    :param api_func: Api client function to call
-    :return:
+    :param test_func: Api client function to call
     """
-    def retry_call_api(*args, **kwargs):
+
+    def retry_call_api(*func_args, **kwargs):
         retry = 1
         max_retry = 6
         while retry <= max_retry:
             try:
-                return test_func(*args, **kwargs)
+                return test_func(*func_args, **kwargs)
             except HTTPError as e:
                 logging.warning('Call retry (%s/%s): ', retry, max_retry)
                 retry += 1
                 time.sleep(2)
                 if retry > max_retry:
-                    logging.error('API unrecoverable error after %s retry', retry-1)
+                    logging.error('API unrecoverable error after %s retry', retry - 1)
                     raise e
+
     return retry_call_api
-         
+
+
 class RestClient(object):
     """
     Base client for interacting with a standard production REST service where the URIs meet a common standard.    
     Most methods are stubs for overriding or decoration by classes that extend this for specific services
     """
-    
+
     jobs = '{}jobs'
     jobs_id = '{}jobs/{}'
-    
+
     def __init__(self, uri):
         assert_http_uri(uri)
         self.uri = uri
-    
+
     @retry_requests
     def submit_job(self, payload):
         """
@@ -49,12 +52,12 @@ class RestClient(object):
         Arguments:
           payload : job input as dict
         """
-        logging.info("Submitting job")        
+        logging.info("Submitting job")
         logging.debug(payload)
         r = requests.post(self.jobs.format(self.uri), json=payload)
         r.raise_for_status()
         return r.json()['job_id']
-    
+
     @retry_requests
     def delete_job(self, job_id, kill=False):
         """
@@ -69,15 +72,15 @@ class RestClient(object):
         r = requests.delete(delete_uri)
         r.raise_for_status()
         return True
-    
+
     @retry_requests
     def list_jobs(self):
         """
         Find all current jobs
-        """ 
+        """
         logging.info("Listing")
         r = requests.get(self.jobs.format(self.uri))
-        r.raise_for_status()    
+        r.raise_for_status()
         return r.json()
 
     @retry_requests
@@ -87,7 +90,7 @@ class RestClient(object):
         The service will respond if it supports this format.
         Arguments:
           job_id - ID of job to retrieve        
-        """ 
+        """
         logging.info("Retrieving job failure for job " + str(job_id))
         r = requests.get(self.jobs_id.format(self.uri, str(job_id)) + '?format=failures')
         r.raise_for_status()
@@ -97,12 +100,12 @@ class RestClient(object):
     @retry_requests
     def retrieve_job_email(self, job_id):
         """
-        Retrieve information on a job using the special format "email" which renders the supplied job in a format suitable
-        for sending by email. 
+        Retrieve information on a job using the special format "email" which renders the supplied job in a format
+        suitable for sending by email.
         The service will respond if it supports this format.
         Arguments:
           job_id - ID of job to retrieve        
-        """ 
+        """
         logging.info("Retrieving job as email for job " + str(job_id))
         r = requests.get(self.jobs_id.format(self.uri, str(job_id)) + '?format=email')
         r.raise_for_status()
@@ -118,10 +121,9 @@ class RestClient(object):
         logging.info("Retrieving results for job " + str(job_id))
         r = requests.get(self.jobs_id.format(self.uri, str(job_id)))
         r.raise_for_status()
-        job = r.json()
-        return job
-    
-    def print_job(self, job, print_results=False, print_input=False):
+        return r.json()
+
+    def print_job(self, a_job, print_results=False, print_input=False):
         """
         Stub utility to print job to logging 
         Arguments:
@@ -129,26 +131,27 @@ class RestClient(object):
           print_results - ignored
           print_input - ignored
         """
-        logging.info(job)
-        
+        logging.info(a_job)
+
     def write_output(self, r, output_file):
         """
         Utility to write response. 
         Arguments:
           job - response object
           output_file - output file handle
-        """                  
-        if(output_file != None):
+        """
+        if output_file is not None:
             with output_file as f:
-                f.write(r.text)  
-    
-    
+                f.write(r.text)
+
+
 if __name__ == '__main__':
-            
+
     parser = argparse.ArgumentParser(description='Run HCs via a REST service')
 
     parser.add_argument('-u', '--uri', help='HC REST service URI', required=True)
-    parser.add_argument('-a', '--action', help='Action to take', choices=['submit', 'retrieve', 'list', 'delete', 'collate'], required=True)
+    parser.add_argument('-a', '--action', help='Action to take',
+                        choices=['submit', 'retrieve', 'list', 'delete', 'collate'], required=True)
     parser.add_argument('-i', '--job_id', help='HC job identifier to retrieve')
     parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
     parser.add_argument('-o', '--output_file', help='File to write output as JSON', type=argparse.FileType('w'))
@@ -156,26 +159,26 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--email', help='User email')
 
     args = parser.parse_args()
-    
-    if args.verbose == True:
+
+    if args.verbose:
         logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     else:
         logging.basicConfig(level=logging.INFO, format='%(message)s')
-    
-    if args.uri.endswith('/') == False:
-        args.uri = args.uri + '/'   
-        
+
+    if not args.uri.endswith('/'):
+        args.uri = args.uri + '/'
+
     client = RestClient(args.uri)
-                
+
     if args.action == 'retrieve':
         job = client.retrieve_job(args.job_id)
         client.print_job(job, print_results=True, print_input=True)
-    
+
     elif args.action == 'list':
-        jobs = client.list_jobs()   
+        jobs = client.list_jobs()
 
     elif args.action == 'delete':
         client.delete_job(args.job_id)
-    
+
     else:
         logging.error("Unknown action {}".format(args.action))
