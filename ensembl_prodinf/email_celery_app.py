41d4cb52e1a93e12c9ab4d02d52e72820680fa00
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-02-15 15:44:21 +0000

    More detailed log message

--- ensembl_prodinf/email_celery_app.py
+++ ensembl_prodinf/email_celery_app.py
@@ -12,7 +12,8 @@ try:
 
     app.config_from_object('email_celery_app_config')
     add_app_handler(app.log, __name__)
-except StandardError:
+except StandardError as e:
+    logging.exception("Error loading email_celery_app_config app config %s", e)
     logging.warning('Celery email requires email_celery_app_config module')
 
 if __name__ == '__main__':
--- ensembl_prodinf/handover_celery_app.py
+++ ensembl_prodinf/handover_celery_app.py
@@ -14,7 +14,8 @@ try:
     app.config_from_object('handover_celery_app_config')
     add_app_handler(app.log, __name__)
 
-except StandardError:
+except StandardError as e:
+    logging.exception("Error loading handover_celery_app_config app config %s", e)
     logging.warning('Celery email requires handover_celery_app_config module')
 if __name__ == '__main__':
     app.start()
